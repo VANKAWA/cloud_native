80913e02eba8dcd2cd6f9cf7800ac7d0b85762a0
VANKAWA <63515224+VANKAWA@users.noreply.github.com>
2021-06-21 21:57:52 -0300

    Add files via upload

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def healthcheck():
         status=200,
         mimetype='application/json'
     )
-
+    app.logger.info('Status request succesfull')
     return response
 
 @app.route('/metrics')
@@ -20,12 +21,15 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
-
+    app.logger.info('Metrics request succesfull')
     return response
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request succesfull')
+
     return "Hello World!"
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
